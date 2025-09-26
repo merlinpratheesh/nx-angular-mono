20400380f4ebb1128ea427fdab72ff7c64e0999b
merlinpratheesh <merlinpratheesh@gmail.com>
2025-09-26 16:21:52 +0530

    test

--- apps/admin/src/app/nx-welcome.ts
+++ apps/admin/src/app/nx-welcome.ts
@@ -432,7 +432,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome admin  Merlin👋
+            Welcome admin  pratheesh
           </h1>
         </div>
         <!--  HERO  -->
